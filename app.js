818ecd8fe497a9b0602c4d26d2ae2ddc5ce4bbb8
Alessandro <alessandro.siqueira@outlook.com>
2025-02-05 21:51:17 -0300

    funcao adicionarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById("amigo").value;
+    if(nomeAmigo === "") {
+        alert("Por favor, insira um nome.")
+    } else {
+        amigos.push(nomeAmigo);
+        console.log(amigos);
+        document.getElementById("amigo").value = "";
+    };
+};
