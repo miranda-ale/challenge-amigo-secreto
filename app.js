b70ad2de4dac2bdde13e926ba7cf778b7d5706ca
Alessandro <alessandro.siqueira@outlook.com>
2025-02-06 21:42:34 -0300

    Inclui comentários no código

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let amigos = [];
 
 function adicionarAmigo() {
     let nomeAmigo = document.getElementById("amigo").value;
-    
-    // Verifica se foi inserido algum nome
-    if(nomeAmigo === "") {
+
+    // Verifica se foi inserido algum nome, caso negativo retorna mensagem
+    if (nomeAmigo === "") {
         alert("Por favor, insira um nome.")
     } else {
         amigos.push(nomeAmigo);
@@ -16,20 +16,26 @@ function adicionarAmigo() {
 
 function atualizarListaAmigos() {
     const listaExibida = document.getElementById("resultado");
+
+    // Limpa a lista antes de ser exibida
     listaExibida.innerHTML = "";
-    for(let i = 0; i < amigos.length; i++){
+
+    // Percorre os elementos da lista e exibe na página
+    for (let i = 0; i < amigos.length; i++) {
         listaExibida.innerHTML += `<li>${amigos[i]}</li>`;
     };
 };
 
 function sortearAmigo() {
-    if(amigos.length === 0) {
+    // Verifica se a lista de amigos possui elementos
+    if (amigos.length === 0) {
         alert("A lista de amigos está vazia");
     } else {
-    let indiceAmigo = Math.floor(Math.random() * amigos.length);
-    const amigoExibido = document.getElementById("resultado");
-    amigoExibido.innerHTML = "";
-    amigoExibido.innerHTML = amigos[indiceAmigo];}
+        let indiceAmigo = Math.floor(Math.random() * amigos.length);
+        const amigoExibido = document.getElementById("resultado");
+        amigoExibido.innerHTML = "";
+        amigoExibido.innerHTML = amigos[indiceAmigo];
+    }
 };
 
 
